9e8d6c731f52209e18920187d000f1131ead69ea
Dong <donggnguyen12@gmail.com>
2022-03-30 21:14:34 -0700

    add link and codeLink for project in Project.js

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -8,31 +8,35 @@ function Projects() {
         {
             name: 'Weather App',
             demoImgLink: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/277554360_1441152686316701_8630898754858162703_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=cYxylNY9QIUAX_ybogZ&_nc_ht=scontent-sea1-1.xx&oh=00_AT-6cg2i5INe3yVPa5LEw9jvf6m-gZax41ReNAKikYe8lw&oe=624666A1',
-            codeImgLink: ''
+            link: ' https://donggo-git.github.io/weather-app/',
+            codeLink: 'https://github.com/donggo-git/weather-app'
         },
         //first slide
         {
             name: 'Commercial website',
             demoImgLink: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/276124445_1441148449650458_4669545148965557671_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=_MISjErgnEIAX-D9FXp&_nc_ht=scontent-sea1-1.xx&oh=00_AT-W7Wei60_BcAEN2bnqQlJslLC0GerO2Ru8AoH8aCN14A&oe=6247E2DA',
-            codeImgLink: '',
-            link: ''
+            link: 'https://donggo-git.github.io/shoes-selling/',
+            codeLink: 'https://github.com/donggo-git/shoes-selling'
         },
         {
             name: 'Netflix Clone App',
             demoImgLink: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/276133909_1441149012983735_4063199915047674009_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=ZkD38YyTRXcAX8nIYEy&_nc_ht=scontent-sea1-1.xx&oh=00_AT972jvQ-jNH6o4WVl_78Gj-aOeCVmTBf_uO1oqdkAPngQ&oe=6247FB5C',
-            codeImgLink: ''
+            link: 'https://donggo-git.github.io/netflix-clone-app/',
+            codeLink: 'https://github.com/donggo-git/netflix-clone-app'
         },
         //last slide
         {
             name: 'Weather App',
             demoImgLink: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/277554360_1441152686316701_8630898754858162703_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=cYxylNY9QIUAX_ybogZ&_nc_ht=scontent-sea1-1.xx&oh=00_AT-6cg2i5INe3yVPa5LEw9jvf6m-gZax41ReNAKikYe8lw&oe=624666A1',
-            codeImgLink: ''
+            link: ' https://donggo-git.github.io/weather-app/',
+            codeLink: 'https://github.com/donggo-git/weather-app'
         },
         //first slide clone
         {
             name: 'Commercial website',
             demoImgLink: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/276124445_1441148449650458_4669545148965557671_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=_MISjErgnEIAX-D9FXp&_nc_ht=scontent-sea1-1.xx&oh=00_AT-W7Wei60_BcAEN2bnqQlJslLC0GerO2Ru8AoH8aCN14A&oe=6247E2DA',
-            codeImgLink: ''
+            link: 'https://donggo-git.github.io/shoes-selling/',
+            codeLink: 'https://github.com/donggo-git/shoes-selling'
         }
     ]
     const [isTransition, setIsTransition] = useState()
@@ -97,9 +101,15 @@ function Projects() {
                                 className='project'
                                 style={index != mainSlideIndex + 1 ? { transform: 'scale(0.7)' } : {}}
                             >
-                                <img src={project.demoImgLink} height='100%' width='100%' />
-                                <p>{project.name}</p>
-                                <p>Source</p>
+                                <a href={project.link} target='blank'>
+                                    <img src={project.demoImgLink} height='100%' width='100%' />
+                                </a>
+                                <a href={project.link} target='blank'>
+                                    <p>{project.name}</p>
+                                </a>
+                                <a href={project.codeLink} target='blank'>
+                                    <p>Source</p>
+                                </a>
                             </div>
                         </div>
                     )
