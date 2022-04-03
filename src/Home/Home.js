04d5a94ed7cb8411b45babe02ea86c975804ebee
Dong <donggnguyen12@gmail.com>
2022-04-02 20:59:54 -0700

    add resume file and contactInfo for Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,17 +12,21 @@ function Home() {
             <div className='header'>
                 <div className='avatar'>
                     <img src="https://media-exp1.licdn.com/dms/image/C5603AQGRDruucqyDKg/profile-displayphoto-shrink_400_400/0/1648146065805?e=1653523200&v=beta&t=iHAAanNmzSERWPKI3DI9Zvm06TNec4_bNixp4ZrTfYo" />
-
                 </div>
                 <div className='name_description'>
                     <h1>Dong Giang Nguyen</h1>
+                    <div className='contactInfo'>
+                        <a href='tel:206-306-5818'>Phone |</a>
+                        <a href="mailto:donggnguyen12@gmail.com"> Email |</a>
+                        <a href="https://github.com/donggo-git"> Github</a>
+                    </div>
                     {/*description */}
                     <p className='description'>
                         Hi there. I’m Dong, a CSC student who passionate with building website.
                         Lack of experience, but work hard and enthusiatic.
                         Looking for internship position.
                     </p>
-                    <button>RESUME <AiOutlineFile /></button>
+                    <button><a href='https://filepost.io/d/SC7nmTGqPY' target="blank" download='Resume.pdf'>RESUME <AiOutlineFile /></a></button>
                 </div>
             </div>
             <Link
