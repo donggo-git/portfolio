e6f143d72b2a962dc6eb29cc8024815c69eec37b
Dong <dlttthvlhhth@gmail.com>
2022-08-26 17:48:22 -0700

    remove Load page and remove unecesary css code

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -50,86 +50,16 @@ function Projects() {
             'A movie app website showing film by genres, trend, let user search film, add film to favorite, play film trailer using third party API: TMDB and react-youtube (note: because I still working on the loading page so there will be a full green page for 1s before seeing the website)',
             'HTML, CSS, JavaScript, Reactjs, Firebase; API: TMDB, react-youtube'
         ),
-        //last slide
-        /*
-        {
-            name: 'Weather App',
-            demoImgLink: 'https://i.postimg.cc/y6vZqYYD/2022-03-28-7.png',
-            link: ' https://donggo-git.github.io/weather-app/',
-            codeLink: 'https://github.com/donggo-git/weather-app',
-            description: 'A simple weather app that can show the weather in a many specific places using data weatherapi'
-        },
-        //first slide clone
-        {
-            name: 'Commercial website',
-            demoImgLink: 'https://i.postimg.cc/nzKq94J2/2022-03-28-5.png',
-            link: 'https://donggo-git.github.io/shoes-selling/',
-            codeLink: 'https://github.com/donggo-git/shoes-selling',
-            description: 'A commercial website that can add to cart, search, filter. Using firebase to store product and items in cart'
-        }*/
     ]
     const showMoreHandler = (project_index) => {
         let newProjectFullDescription = [...projectFullDescription]
         newProjectFullDescription[project_index] = !newProjectFullDescription[project_index]
         setProjectFullDescription(newProjectFullDescription)
     }
-    const [isTransition, setIsTransition] = useState()
-    const [mainSlideIndex, setMainSlideIndex] = useState(0)
-    const windowWidth = window.innerWidth;
 
-    const handlePre = () => {
-        setIsTransition(true)
-        //when user go into the first slide it will change back to last slide
-        if (mainSlideIndex == 0) {
-            setIsTransition(false)
-            setMainSlideIndex(imgSlide.length - 1)
-            setTimeout(() => {
-                setIsTransition(true)
-                setMainSlideIndex(pre => pre - 1)
-            }, 0.01)
 
-        };
-        setMainSlideIndex(pre => pre - 1)
-    }
-    const handleNext = () => {
-        setIsTransition(true)
-        //when user go into the first slide it will change back to last slide
-        //when user use website on the computer
-        if (windowWidth > 1000) {
-            if (mainSlideIndex == imgSlide.length - 3) {
-                setIsTransition(false)
-                setMainSlideIndex(-1)
-                setTimeout(() => {
-                    setIsTransition(true)
-                    setMainSlideIndex(pre => pre + 1)
-                }, 0.01)
-
-            }
-            else setMainSlideIndex(pre => pre + 1)
-        }
-        //when user use website on the phone or ipad
-        else {
-            if (mainSlideIndex == imgSlide.length - 1) {
-                setIsTransition(false)
-                setMainSlideIndex(1)
-                setTimeout(() => {
-                    setIsTransition(true)
-                    setMainSlideIndex(pre => pre + 1)
-                }, 0.01)
-            }
-            else setMainSlideIndex(pre => pre + 1)
-        }
-    }
-
-    const handleDot = (index) => {
-        setIsTransition(true)
-        if (windowWidth > 1000) {
-            setMainSlideIndex(index - 1)
-        }
-        else setMainSlideIndex(index)
-    }
     return (
-        <ProjectsList className='page' name="PROJECTS">
+        <ProjectsList name="PROJECTS">
             {imgSlide.map((project, project_index) => (
                 <ProjectContainer key={project._name}>
                     <ProjectImage src={project._demoImgLink} project_index={project_index} />
@@ -157,88 +87,6 @@ function Projects() {
                     </ProjectContent>
                 </ProjectContainer>
             ))}
-            {/*
-            <div
-                className=' Projects__btn__container Projects__btn__container--Pre'
-                onClick={() => handlePre()}
-            >
-                <AiOutlineLeft />
-            </div>
-            <div
-                className=' Projects__btn__container Projects__btn__container--Next'
-                onClick={() => handleNext()}
-            >
-                <AiOutlineRight />
-            </div>
-            <div className='slider__container'>
-                <div
-                    className='slider'
-                    style={{
-                        transform: `translate(${
-                                //for pc or laptop
-                                windowWidth > 1000 ?
-                                    -mainSlideIndex * (80 / 3) :
-                                    //for tablet
-                                    windowWidth > 800 ?
-                                        -mainSlideIndex * 50 :
-                                        //for phone
-                                        -mainSlideIndex * (80)
-                            }vw,0%)`,
-                            transition: isTransition ? 'all 0.4s' : ''
-                    }}
-                >
-                            {imgSlide.map((project, index) => (
-                                (
-                                    <div
-                                        className='project__container'
-
-                                    >
-                                        <div
-                                            className='project'
-                                            style={
-                                                index != mainSlideIndex + 1 && windowWidth > 1000 ?
-                                                    { transform: 'scale(0.7)' } : {}
-                                            }
-                                        >
-
-                                            <a href={project.link} target='blank'>
-                                                <img src={project.demoImgLink} height='100%' width='100%' />
-                                            </a>
-                                            <div className='project__content'>
-                                                <p className='project__content--name'>{project.name}</p>
-                                                <p> {project.description}</p>
-                                                <div className='project__buttons'>
-                                                    <a href={project.codeLink} target='blank'>
-                                                        view source
-                                                    </a>
-                                                    <a href={project.link} target='blank'>
-                                                        visit website
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            ))}
-                        </div>
-
-                    </div>
-                    <div className='slider__dots'>
-                        {imgSlide.slice(1, imgSlide.length - 1).map((dot, index) => (
-                            <div
-                                className='slider__dot'
-                                style={
-                                    //dot active when user click on the same slide with the dot
-                                    index == mainSlideIndex ||
-                                        (index == 0 && mainSlideIndex == 3) ||
-                                        (index == 1 && mainSlideIndex == imgSlide.length - 1)
-                                        ? { backgroundColor: 'rgba(243, 243, 243, 1)' } : {}
-                                }
-                                onClick={() => handleDot(index)}
-                            />
-                        ))}
-                    </div>
-*/}
         </ProjectsList>
     )
 }
